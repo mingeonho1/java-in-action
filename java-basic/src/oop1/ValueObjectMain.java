e3f05a7d5c55bec5d190ebad457dacc019c26ed1
HOYA <als77770@gmail.com>
2023-12-13 16:02:14 +0900

    객체 지향 프로그래밍
    절차 지향은 데이터와 해당 데이터에 대한 처리 방식이 분리되어 있다. 반면 객체 지향에서는 데이터와 그 데이터에 대한 행동(메서드)이 하나의 '객체'안에 함께 포함되어 있다.

---
+++ java-basic/src/oop1/ValueObjectMain.java
@@ -0,0 +1,13 @@
+package oop1;
+
+public class ValueObjectMain {
+
+    public static void main(String[] args) {
+        ValueObject valueObject = new ValueObject();
+
+        valueObject.add();
+        valueObject.add();
+        valueObject.add();
+    }
+
+}
